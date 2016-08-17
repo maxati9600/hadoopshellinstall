cabe4d580a31057cee758154a67139ba07937cb6
min <maxati9600@gmail.com>
2016-08-17 14:54:42 +0800

    Change krb5 acl config file adding other host name

--- krb5.sh
+++ krb5.sh
@@ -6,34 +6,35 @@
 passwd="q123456"
 username="min"
 master_uname="master"
+krb_realm="master"
 slave_uname="slave"
 slave_num=2
 dir="/opt/key"
 #gen slave array. "slave1 skave2 slave3 ...."
-for sslave in `seq 1 1 $slave_num`;
+for sslave in `seq 1 1 ${slave_num}`;
 do
-	slave=$slave" "$slave_uname$sslave
+	slave=${slave}" "${slave_uname}${sslave}
 done
 ##########################################
 #gen all set array
-all=$master_uname" "$slave
+all=${master_uname}" "${slave}
 #########################
 #Check require package
 require_package="krb5-kdc krb5-admin-server haveged vim"
-for rp in $require_package ; do
+for rp in ${require_package} ; do
 	tmp=""
-	tmp=`dpkg-query -l |awk '{print $2}'|grep $rp`
-	if [ -z "$tmp" ];
+	tmp=`dpkg-query -l |awk '{print $2}'|grep ${rp}`
+	if [ -z "${tmp}" ];
 	then
-		rpackage=$rpackage" "$rp
+		rpackage=${rpackage}" "${rp}
 	fi		
 done
 
-if [ -n "$rpackage" ]; then
+if [ -n "${rpackage}" ]; then
 	echo "================================================"
 	echo "Something missing...Let automatic install it"
 	echo "================================================"
-	apt install $rpackage -y
+	apt install ${rpackage} -y
 fi
 
 echo "================================================"
@@ -54,27 +55,36 @@ else
         exit
 fi
 
-echo -e "$passwd\n$passwd\n" |krb5_newrealm
-echo "*/admin@master *" >>/etc/krb5kdc/kadm5.acl
-echo -e "$passwd\n$passwd" |kadmin.local -q "addprinc admin/admin"
+echo -e "${passwd}\n${passwd}\n" |krb5_newrealm
+for host in ${all}; do
+	echo "*/${host}@${krb_realm} *\n" >>/etc/krb5kdc/kadm5.acl
+done
+echo -e "${passwd}\n${passwd}" |kadmin.local -q "addprinc admin/admin"
 service krb5-admin-server restart
-echo -e "$passwd" |kinit admin/admin
+echo -e "${passwd}" |kinit admin/admin
 klist
 #create all key for all host
 echo "======================================================================="
 echo "generate key for all machine"
 echo "======================================================================="
 for host in $all ;do
-	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey hdfs/$host@master"
-	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey mapred/$host@master"
-	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey HTTP/$host@master"
-	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey yarn/$host@master"
-	kadmin.local -q "xst -norandkey -k hdfs.keytab hdfs/$host@master"
-	kadmin.local -q "xst -norandkey -k mapred.keytab mapred/$host@master"
-	kadmin.local -q "xst -norandkey -k HTTP.keytab HTTP/$host@master"
-	kadmin.local -q "xst -norandkey -k yarn.keytab yarn/$host@master"
+	mkdir -p ${dir}"/"${host}
+#	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey hdfs/$host@master"
+        kadmin.local -q "addpronc -randkey client/${host}@${krb_realm}"
+	kadmin.local -q "addpronc -randkey hdfs/${host}@${krb_realm}"
+        kadmin.local -q "addpronc -randkey mapred/${host}@${krb_realm}"
+        kadmin.local -q "addpronc -randkey HTTP/${host}@${krb_realm}"
+        kadmin.local -q "addpronc -randkey yarn/${host}@${krb_realm}"
+#	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey mapred/$host@master"
+#	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey HTTP/$host@master"
+#	echo -e "$passwd" |kadmin -p admin/admin -q "addprinc -randkey yarn/$host@master"
+	kadmin.local -q "xst -norandkey -k ${dir}/${host}/hdfs.keytab hdfs/${host}@${krb_realm} client/${host}@${krb_realm}"
+        kadmin.local -q "xst -norandkey -k ${dir}/${host}/mapred.keytab mapred/${host}@${krb_realm} client/${host}@${krb_realm}"
+        kadmin.local -q "xst -norandkey -k ${dir}/${host}/HTTP.keytab HTTP/${host}@${krb_realm} client/${host}@${krb_realm}"
+        kadmin.local -q "xst -norandkey -k ${dir}/${host}/yarn.keytab yarn/${host}@${krb_realm} client/${host}@${krb_realm}"	
 done
 #########
+exit 
 #move key in /opt/key and chang owner
 echo "======================================================================="
 echo "move key to /opt/key"
