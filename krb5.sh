9011d2e9011b3721558124b434e676f10a77d769
min <maxati9600@gmail.com>
2016-08-23 10:55:11 +0800

    add slave ker5-user install

--- krb5.sh
+++ krb5.sh
@@ -103,7 +103,7 @@ for sHost in ${slave} ; do
 	echo "Start copy to "${sHost}
 	echo "mkdir for hadoop "${sHost}":"${dir}" owner:"${username}
 	echo "You might need to enter ${sHost}'s root password "
-	ssh -t ${username}@${sHost} "sudo mkdir -p ${dir} && sudo chown ${username}.${username} ${dir}"
+	ssh -t ${username}@${sHost} "sudo mkdir -p ${dir} && sudo chown ${username}.${username} ${dir} && sudo apt-get install krb5-user krb5-config -y"
 	echo "file copy"
 	scp $dir/${sHost}/* ${username}@${sHost}:${dir}/ 
 done
